ff03566a8102c6cf1ad41d2ae4ec320f5a899e7c
Kelves <kelves_16@hotmail.com>
2020-10-21 21:53:16 -0300

    fix/deletion: deleted unnecessary code, reduced sleep time and  renamed some output files.

--- perftool.py
+++ perftool.py
@@ -6,11 +6,11 @@ from time import process_time_ns as timer_ns
 from timeit import default_timer as timer
 from datetime import timedelta as delta
 # to call the respective routines
-import subprocess #as ps
+import subprocess as ps
 
 # local imports
 import udfactorial as uf
-import mldfactorial as mf
+from math import factorial as mf
 
 # third party module that reports time mesurements
 from timebudget import timebudget
@@ -19,70 +19,26 @@ from timebudget import timebudget
 timebudget.set_quiet()
 timebudget.report_at_exit()
 
-#class Test:
-#    def __init__(self, vector):
-#        self.vector = open("vector.txt", "r")
-#
-#    def run_test(self, recordFile):
-#        self.vector.seek(0,0)
-#        self.recordFile.seek(0,2)
-#        for num in self.vector.read().split():
-#
-#            start = timer_ns()
-#            fac = self.factorial(int(num))
-#            end = timer_ns()
-#            dt = end - start
-#            totalTime += dt
-#
-#            recordFile.write(f"{num}   {dt}\n")
-#
-#            print(f"factorial of {num}: {fac}")
-#            print(f"took {dt} nanoseconds")
-#            sleep(0.2)
-#
-#        self.vector.close()
-#        print(f"Total time elapsed: {totalTime} nanoseconds")
-#        self.recordFile.close()
-#
-#
-#
-#class PyUserTest(Test):
-#    def _init_(self, recordFile):
-#        self.recordFile = open("user_fac_marks.txt", "w")
-#
-#    @timebudget
-#    def factorial(self, n):
-#        return uf.udfactorial(n)
-#
-#
-#class VMTest(Test):
-#    def __init__(self):
-#        pass
-
 @timebudget
 def user_defined_fac(n):
     return uf.udfactorial(n)
 
 @timebudget
 def mathlib_defined_fac(n):
-    return mf.factorial(n)
+    return mf(n)
 
 @timebudget
 def vm_defined_fac(n):
-    subprocess.run(["./vm_code/hack_machine/CPUEmulator.sh",
-                    "./vm_code/test/Factorial.tst",
-                    "2&>1 >/dev/null"],
-                    stdin=None,  # testing
-                    stdout=None, # testing
-                    stderr=None, # testing
-                    capture_output=True,
-                    text=True)
-    #return subprocess.CompletedProcess(args=args, returncode=returncode)
+    ps.run(["./vm_code/hack_machine/CPUEmulator.sh",
+            "./vm_code/test/Factorial.tst",
+            "2&>1 >/dev/null"],
+            capture_output=True,
+            text=True)
 
 def test_user_factorial():
-    results = open("user_fac_marks.txt", "w")
+    results = open("./results/vector_nxt_user.txt", "w")
     results.seek(0,2)
-    vector = open("vector.txt", "r")
+    vector = open("vector_entries.txt", "r")
     vector.seek(0,0)
 
     totalTime = 0
@@ -95,20 +51,18 @@ def test_user_factorial():
         dt = end - start
         totalTime += dt
 
-        results.write(f"{num}   {dt}\n")
-
-        print(f"factorial of {num}: {fac}")
-        print(f"took {dt} nanoseconds")
-        sleep(0.2)
+        results.write(f"{num} {dt}\n")
+        print(f"factorial of {num} took {dt} nanoseconds")
+        sleep(0.05)
 
     vector.close()
     print(f"Total time elapsed: {totalTime} nanoseconds")
     results.close()
 
 def test_math_factorial():
-    results = open("mathlib_fac_marks.txt", "w")
+    results = open("./results/vector_nxt_mathlib.txt", "w")
     results.seek(0,2)
-    vector = open("vector.txt", "r")
+    vector = open("vector_entries.txt", "r")
     vector.seek(0,0)
 
     totalTime = 0
@@ -121,39 +75,35 @@ def test_math_factorial():
         dt = end - start
         totalTime += dt
 
-        results.write(f"{num}   {dt}\n")
-
-        print(f"factorial of {num}: {fac}")
-        print(f"took {dt} nanoseconds")
-        sleep(0.2)
+        results.write(f"{num} {dt}\n")
+        print(f"factorial of {num} took {dt} nanoseconds")
+        sleep(0.05)
 
     vector.close()
     print(f"Total time elapsed: {totalTime} nanoseconds")
     results.close()
 
 def test_vm_factorial():
-    results = open("vm_fac_marks.txt", "w")
+    results = open("./results/vector_nxt_vm.txt", "w")
     results.seek(0,2)
-    vector = open("vector.txt", "r")
+    vector = open("vector_entries.txt", "r")
     vector.seek(0,0)
 
     totalTime = 0
 
     for num in vector.read().split():
 
-        subprocess.run(["./asmmodifier.sh", str(num)])
+        ps.run(["./asmmodifier.sh", str(num)])
 
         start = timer_ns()
-        fac = vm_defined_fac(int(num))
+        vm_defined_fac(int(num))
         end = timer_ns()
         dt = end - start
         totalTime += dt
 
-        results.write(f"{num}   {dt}\n")
-
-        print(f"factorial of {num}: {fac}")
-        print(f"took {dt} nanoseconds")
-        sleep(0.2)
+        results.write(f"{num} {dt}\n")
+        print(f"factorial of {num} took {dt} nanoseconds")
+        sleep(0.05)
 
     vector.close()
     print(f"Total time elapsed: {totalTime} nanoseconds")
